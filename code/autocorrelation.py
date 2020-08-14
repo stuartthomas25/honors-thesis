Fix ensemble averaging issue

code/autocorrelation.py
@@ -13,17 +13,17 @@ from UWerr import UWerr
 import pickle
 
 SEED = True
-L = 64
+L = 16
 lam = 0.5
-m02s = [-0.72]
+m02s = [-0.80]
 
 # sweeps = 10**5
-cluster_method = None
+cluster_method = None if "nowolf" in sys.argv else WOLFF
 thermalization = 10**4
-record_rate = 1
+record_rate = 100
 cluster_rate = 5
 
-measurements = 10**4
+measurements = 100
 
 sweeps = thermalization + record_rate * measurements
 # print(sweeps, "sweeps")
@@ -33,7 +33,7 @@ quantities = ['data', 'magnetization', 'susceptibility', 'binder_cumulant', 'act
 def main():
 
     if RANK==0:
-        recorders = [Recorder(quantities, thermalization=thermalization, rate=record_rate, gif=False) for _ in m02s]
+        recorders = [Recorder(quantities, thermalization=thermalization, rate=record_rate) for _ in m02s]
     else:
         recorders = [None for _ in m02s]
 
@@ -46,7 +46,6 @@ def main():
 
         rw = RandomWalk(l)
         start = time()
-        if RANK==0: recorder.record(l)
         rw.run(sweeps, cluster_method=cluster_method,  cluster_rate=cluster_rate, recorder=recorder, progress=True)
 
         if RANK==0:
@@ -59,6 +58,7 @@ def main():
         recorders[i].record(l)
         pickle.dump(recorders, open('data/converge.pickle', 'wb'))
         print("Saved pickle")
+        print("Count:",recorder.record_count)
         if recorders[i].gp is not None:
             recorders[i].save_gif('plots/lattice.gif')
             print("Saved gif")
@@ -73,10 +73,12 @@ def view():
         data = np.array(recorder.values['data'])
         phi_sq = np.mean(data**2, axis=(1,2))
 
-        UWerr(phi_sq, name=f"$\\langle \\phi^2 \\rangle$: $L={L}$, $\\lambda={lam}$, $m_0^2={m02s[i]}$, {sweeps} sweeps, {thermalization} sweep thermalization, recording every {record_rate} sweeps", plot=True, whole_plot=True)
+        print(recorder.mean('magnetization'))
+        #UWerr(phi_sq, name=f"$\\langle \\phi^2 \\rangle$: $L={L}$, $\\lambda={lam}$, $m_0^2={m02s[i]}$, {sweeps} sweeps, {thermalization} sweep thermalization, recording every {record_rate} sweeps", plot=True, whole_plot=True)
 
         fig, axes = plt.subplots(4, 1, figsize=(16,16))
         sweep_x = np.arange(recorder.record_count)
+        print("Count:",recorder.record_count)
 
         phi_broken =np.sqrt(-m02s[i]/lam)
 
@@ -112,52 +114,6 @@ def view():
         axes[0].set_title(f"$\\langle \\phi \\rangle$ histograms: $L={L}$, $\\lambda={lam}$, {sweeps} sweeps, {thermalization} sweep thermalization, recording every {record_rate} sweeps")
         axes[-1].set_xlabel(r"$\langle \phi \rangle$")
         plt.show()
-
-
-# # Test gradient flow
-    # recorder = Recorder()
-
-    # def laplacian(a):
-        # return np.gradient(np.gradient(a, axis=0), axis=0) + np.gradient(np.gradient(a, axis=1), axis=1)
-
-    # last_rho = None
-    # for tau in np.linspace(0.,0.005,100):
-        # rho = l.rho(tau)
-        # recorder.save(rho)
-        # if last_rho is not None:
-            # dt_data = rho.data - last_rho.data
-            # grad_data = laplacian(rho.data)
-        # last_rho = rho
-    # recorder.plot(title=f"Effect of gradient flow on observables: $L={L}$, $\\lambda={lam}$, $\\mu_0^2={m02}$", fname="gradient_flow", xlabel=r'$\tau$ (flow time)', show=True)
-
-    # recorder.save_gif("plots/gradient_flow.gif", fps=30)
-
-    # quit() # just testing the gradient flow, so stop here
-
-    # # set the site assignments (factor of 2 for checkerboard)
-
-
-    # cluster_rate = 1
-    # record_rate = 1
-    # thermalization = 1
-
-    # recorder = Recorder()
-    # recorder.save(l)
-
-    # rw = RandomWalk(l)
-
-    # start = time()
-
-    # rw.run(sweeps, cluster_method, cluster_rate, record_rate, thermalization, recorder)
-
-    # if RANK==0:
-        # exec_time = time() - start
-        # print(f"{exec_time}")
-
-        # recorder.save_gif("plots/lattice_visualization.gif")
-
-        # recorder.plot(title=f"Monte Carlo Simulation of $\phi^4$ Model using Metropolis and Wolff Algorithms, $L={L}$, $\\lambda={lam}$, $\\mu_0^2={m02}$, $t={exec_time:.1f}s$", fname="parallel.png", show=('-S' in dopts))
-
 if __name__=="__main__":
     if 'view' in sys.argv:
         view()

code/mcmc/recorder.py
@@ -10,19 +10,19 @@ import numpy as np
 
 
 class Quantity(object):
-    def __init__(self, f, label):
+    def __init__(self, f, label=None):
         self.f = f
         self.label = label
 
     def __call__(self, *args, **kwargs):
         return self.f(*args, **kwargs)
 
-
 class Recorder(object):
 
-    quantities = {}
+    primary_observables = {}
+    derived_observables = {}
 
-    def __init__(self, quantities, rate=1, thermalization=0, gif = False):
+    def __init__(self, rate=1, thermalization=0, gif = False):
         if gif:
             self.gp = GifProducer()
         else:
@@ -32,21 +32,28 @@ class Recorder(object):
         self.rate = rate
 
         self.values = {}
-        for quant in type(self).quantities:
-            if quant in quantities:
-                self.values[quant] = []
+
+        for obsrv in type(self).primary_observables:
+            self.values[obsrv] = []
 
         self.record_count = 0
 
         # print("Recorder",Recorder.quantities)
 
     @classmethod
-    def quantity(cls, label):
+    def derived_observable(cls, label):
         def decorator(f):
             name = f.__name__
-            cls.quantities[name] = Quantity(f, label)
+            cls.derived_observables[name] = Quantity(f, label)
             return f
         return decorator
+
+    @classmethod
+    def primary_observable(cls, f):
+        name = f.__name__
+        cls.primary_observables[name] = Quantity(f)
+        return f
+
 
     def num_measurements(self, sweeps):
         return (sweeps - self.thermalization) // self.rate - 1
@@ -55,10 +62,9 @@ class Recorder(object):
         for v in self.values:
             self.values[v] = []
 
-
     def record(self, lat):
         for val, arr in self.values.items():
-            arr.append(type(self).quantities[val] (lat))
+            arr.append(type(self).primary_observables[val] (lat))
         if self.gp is not None:
             self.gp.save_lat(lat)
         self.record_count += 1
@@ -66,70 +72,54 @@ class Recorder(object):
     def save_gif(self, fname, fps=3):
         self.gp.save(fname, fps)
 
-    # def plot(self, title="", fname=None, show=True, xlabel="Sweep"):
-        # if fname is None:
-            # fname = "Figure.png"
-        # x = self.iter_index
-        # fig, (ax1, ax2, ax3, ax4) = plt.subplots(4,1, figsize=(18,12), sharex = True)
-        # ax1.plot(x, self.magnetizations)
-        # ax2.plot(x, self.energies)
-        # ax3.plot(x, self.susceptibilities)
-        # ax4.plot(x, self.binder_cums)
+    @property
+    def means(self):
+        return {k : sum(self.values[k]) / self.record_count for k in self.values}
 
-        # ax1.set_ylabel("Magnetization")
-        # ax2.set_ylabel("Total Action")
-        # ax3.set_ylabel("Susceptibility")
-        # ax4.set_ylabel("Binder Cumulant")
-        # ax4.set_ylim((-0.1,1.1))
-        # ax4.set_xlabel(xlabel)
-        # # ax3.set_ylim((-0.05,1.05))
-        # ax1.set_title(title)
-        # plt.savefig('plots/'+fname)
-        # if show:
-            # plt.show()
+    @property
+    def errors(self):
+        stderrs = {}
+        for key in self.values:
+            nparr = np.array(self.values[key])
+            stddev = np.std(nparr)
+            stderrs[key] = stddev / sqrt(nparr.size)
 
-    def mean(self, key):
-        arr = self.values[key]
-        return sum(arr) / len(arr)
+        return stderrs
 
-    def error(self, key):
-        if key not in self.values:
-            raise Exception("Quantity key not found")
-        nparr = np.array(self.values[key])
-        stddev = np.std(nparr)
-        stderr = stddev / sqrt(nparr.size)
+    def derived_value(self, key):
+        return type(self).derived_observables[key] (**self.means)
 
-        return stderr
-
-@Recorder.quantity(r"$|\langle \phi \rangle|$")
-def magnetization(lat):
+@Recorder.primary_observable
+def phi(lat):
     return np.sum(lat.data) / lat.size
 
-@Recorder.quantity(r"$\phi$")
-def data(lat):
-    return lat.data
+@Recorder.primary_observable
+def abs_phi(lat):
+    return abs(np.sum(lat.data)) / lat.size
+
+@Recorder.primary_observable
+def phi2(lat):
+    return np.sum(lat.data**2) / lat.size
+
+@Recorder.primary_observable
+def phi4(lat):
+    return np.sum(lat.data**4) / lat.size
 
 
-@Recorder.quantity(r"$|\langle \phi \rangle|$")
-def abs_magnetization(lat):
-    return abs(np.sum(lat.data) / lat.size)
 
 
-@Recorder.quantity(r"$U$")
-def binder_cumulant(lat):
-    phi_sq = np.sum(lat.data**2) / lat.size
-    phi_qu = np.sum(lat.data**4) / lat.size
-    return 1 - phi_qu / (3 * phi_sq**2)
+@Recorder.derived_observable(r"$\langle|\bar \phi|\rangle$")
+def magnetization(abs_phi, **kwargs):
+    return abs_phi
 
-@Recorder.quantity(r"$\chi$")
-def susceptibility(lat):
-    m =  np.sum(lat.data) / lat.size
-    m2 = np.sum(lat.data**2) / lat.size
-    return (m2 - m**2)
+@Recorder.derived_observable(r"$U$")
+def binder_cumulant(phi4, phi2, **kwargs):
+    return 1 - phi4 / (3 * phi2**2)
 
-@Recorder.quantity(r"$S$")
-def action(lat):
-    return lat.action / lat.size
+@Recorder.derived_observable(r"$\chi$")
+def susceptibility(phi2, abs_phi, **kwargs):
+    return phi2 - abs_phi**2
+
 
 class GifProducer(object):
     def __init__(self):
@@ -142,3 +132,4 @@ class GifProducer(object):
 
     def save(self, fn, fps=3):
         imageio.mimwrite(fn, self.frames, fps=fps)
+